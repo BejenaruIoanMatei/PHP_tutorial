027a1639f68e7f90254e56ce42d96fd282634f4f
BejenaruIoanMatei <bejematei@gmail.com>
2025-08-30 19:07:19 +0300

    4: Array Data Type

--- index.php
+++ index.php
@@ -1,23 +1,50 @@
 <?php
 
-// Heredoc
+// Arrays
 
-$x = 1;
-$y = 2;
+$programmingLanguages = [
+    'php' => [
+        'creator'=> 'mr saxobeat',
+        'extension'=> true,
+        'versions'=>[
+            ['version' => '8','releaseDate'=> 'Nov 26, 2023'],
+            ['version'=> '7,4','releaseDate'=> 'Dec 21, 2019'],
+        ]
+    ]
+];
 
-$text = <<<TEXT
-Line {$x}
-Line {$y}
-TEXT;
+echo '<pre>';
+print_r($programmingLanguages);
+echo '</pre>';
 
-echo nl2br($text);
+$programmingLanguages[] = '1.15';
 
-// Nowdoc
+echo '<pre>';
+print_r($programmingLanguages);
+echo '</pre>';
 
-$text = <<<'TEXT'
-Line {$x}
-Line {$y}
-TEXT;
+echo $programmingLanguages['php']['extension'];
 
-echo '<br />';
-echo nl2br($text);
+$array = [1, 2, 3];
+
+echo array_pop($array);
+
+echo array_shift($array);
+
+echo '<pre>';
+print_r($array);
+echo '</pre>';
+
+unset($array[0]);
+
+echo '<pre>';
+print_r($array);
+echo '</pre>';
+
+$x = 5;
+
+var_dump((array)$x);
+
+$x = (array) $x;
+
+var_dump(array_key_exists('0', $x));
