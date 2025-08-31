60fd9f88af41e345ec3ed5d44d0948c1cebe69cb
BejenaruIoanMatei <bejematei@gmail.com>
2025-08-31 14:44:34 +0300

    6: Laracasts - Functions and Filters

--- laracasts/index.php
+++ laracasts/index.php
@@ -8,18 +8,66 @@
 </head>
 <body>
     <?php 
-        $name = "Dark Matter";
-        $read = true;
+        $books = [
+            [
+                'name' => 'Book 1',
+                'author'=> 'Author Book 1',
+                'price'=> 20,
+                'releaseYear'=> 1968,
+                'url'=> 'example.com',
+            ],
+            [
+                'name' => 'Book 2',
+                'author'=> 'Author Book 2',
+                'price'=> 39,
+                'releaseYear'=> 2021,
+                'url'=> 'example.com',
+            ],
+            [
+                'name' => 'Book 3',
+                'author'=> 'Author Book 3',
+                'price'=> 18,
+                'releaseYear'=> 2011,
+                'url'=> 'example.com',
+            ]
+        ];
 
-        if ($name) {
-                echo '<h1>' . "I read {$name}" . '</h1>';  
-        } else {
-            echo "I dont know bruv";
+        function filterByAuthor(array $books, string $author): array {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
         }
     ?>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <?php if ($book['author'] === 'Author Book 1') : ?>
+            <h1>
+                <li>
+                    <?= $book['name'] ?> by <?= $book['author'] ?>
+                    <a href="<?= $book['url'] ?>">
+                    <br>
+                    Buy <?= $book['name'] ?>
+                    Here !
+                </a>
+                </li>
+            </h1>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
 
-    <h1>
-        You have read 'Whatev'
-    </h1>
+    <p>
+        <?php
+            foreach (filterByAuthor($books, 'Author Book 1') as $element) :
+                echo "<h1>{$element['author']}</h1>";
+            endforeach;
+        ?>
+    </p>
+
 </body>
 </html>
