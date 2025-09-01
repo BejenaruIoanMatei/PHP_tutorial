e9c75266604d662d9b6c928bf559e067b7e3386a
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-01 14:39:49 +0300

    9: Laracasts - PHP Router

--- laracasts/index.php
+++ laracasts/index.php
@@ -1,5 +1,12 @@
 <?php 
 
-$heading = "Home";
- 
-require "views/index.view.php";
+require "functions.php";
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if ($uri === '/php_tutorial/laracasts/') {
+    require '../laracasts/controllers/index.php';
+} else if ($uri === '/php_tutorial/laracasts/about.php'){
+    require '../laracasts/controllers/about.php';
+}
+
