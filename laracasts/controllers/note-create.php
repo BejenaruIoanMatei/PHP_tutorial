fb248f85678d007046a3c8c824b19f371089582b
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-04 13:09:05 +0300

    17: Laracasts - SQL Injection

--- laracasts/controllers/note-create.php
+++ laracasts/controllers/note-create.php
@@ -1,7 +1,13 @@
 <?php
 
+$config = require BASE_PATH . 'config.php';
+$db = new Database($config['database']);
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 
 view("note-create.view.php", [
