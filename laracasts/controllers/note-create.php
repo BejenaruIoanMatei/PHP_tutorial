874a454fb95de9c493be58e9cfff9b456fc8d6d5
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-04 14:54:14 +0300

    18: Laracasts - Form Validation

--- laracasts/controllers/note-create.php
+++ laracasts/controllers/note-create.php
@@ -3,13 +3,31 @@
 $config = require BASE_PATH . 'config.php';
 $db = new Database($config['database']);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+
+    if (strlen($_POST['body']) === 0)
+    {
+        $errors['body'] = 'A body is required.';
+    }
+
+    if (strlen($_POST['body']) > 1000)
+    {
+        $errors['body'] = 'The body can\'t be more than 1,000 characters.';
+    }
+
+    if (empty($errors))
+    {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 view("note-create.view.php", [
-    'heading' => 'Create a New Note'
+    'heading' => 'Create a New Note',
+    'errors' => $errors,
 ]);
--- laracasts/views/note-create.view.php
+++ laracasts/views/note-create.view.php
@@ -9,10 +9,15 @@
         <div class="col-span-full">
           <label for="body" class="block text-sm font-medium text-gray-900">Note</label>
           <div class="mt-2">
-            <textarea id="body" name="body" rows="6" required
-              class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"></textarea>
+            <textarea id="body" name="body" rows="6" placeholder="Write your note here..." 
+              class="block w-full rounded-md bg-white px-3 py-2 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
+              required><?= $_POST['body'] ?? ''?></textarea>
           </div>
-          <p class="mt-3 text-sm text-gray-600">Write your note here.</p>
+          <?php if (isset($errors['body'])) : ?>
+                <p class="text-red-500 text-xs mt-2">
+                  <?= $errors['body'] ?>
+                </p>
+          <?php endif ?>
         </div>
       </div>
 
