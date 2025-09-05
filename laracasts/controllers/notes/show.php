3e0746d606ccb428d421f45c5ea46343a8afe16d
BejenaruIoanMatei <bejematei@gmail.com>
2025-09-05 18:52:57 +0300

    21: Laracasts - Note Delete

--- laracasts/controllers/notes/show.php
+++ laracasts/controllers/notes/show.php
@@ -1,18 +1,41 @@
 <?php
 
 $config = require BASE_PATH . 'config.php';
+
+$BASE_URL_NOTES = BASE_PATH . 'laracasts/controllers/notes';
+
 $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-authorize($note['user_id'] === $currentUserId);
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note,
-]);
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes where id=:id', [
+        'id' => $_GET['id']
+    ]);
+
+    header("location: /php_tutorial/notes");
+    
+    exit();
+
+} else {
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note,
+    ]);
+
+}
 
